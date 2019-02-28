b82ca8ebe90e5a69a7807a72a1068636fb6fdf51
Gerrudo <43029469+Gerrudo@users.noreply.github.com>
2019-02-28 22:21:11 +0000

    Added schedule using node-schedule

--- send-cat.js
+++ send-cat.js
@@ -1,41 +1,46 @@
 //SendFurryFriend, Thomas Turner 27/02/19
 
+//Dependancies
+var schedule = require('node-schedule');
 var request = require("request");
+var webhook = require("webhook-discord");
 
-var options = { method: 'GET',
-  url: 'https://api.thecatapi.com/v1/images/search',
-  headers: 
-   { 'Token': 'b20162f6-21d8-4fc7-aede-20d74f9a1b51',
-     'cache-control': 'no-cache' } };
+console.log('>>>cat-send 1.1 running')
+console.log('>>>Set to msg at 08:00')
 
-     console.log('>>>Request Sent...')
+//How often the message is sent, in crontab format
+var j = schedule.scheduleJob('0 8 * * *', function(){
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+  var options = { method: 'GET',
+    url: 'https://api.thecatapi.com/v1/images/search',
+    headers: 
+    { 'Token': 'b20162f6-21d8-4fc7-aede-20d74f9a1b51',
+      'cache-control': 'no-cache' } };
 
-  console.log('>>>Body:', body);
+      console.log('>>>Request Sent...')
 
-  var jsonObject = JSON.parse(body);
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-  console.log('>>>URL:',jsonObject[0].url);
-  
-  const webhook = require("webhook-discord");
-  //Webhook goes here
-  const Hook = new webhook.Webhook("https://discordapp.com/api/webhooks/549692863576014888/sVibcWVg1q79zZORCS2J--uTdef5p5tGxEzoVlX7044U2TsAlBesmB9jEnFVLg3D3VF6");
-  //Your Message goes here
-  const msg = new webhook.MessageBuilder()
-                  .setName("Pet of the Day")
-                  .setColor("#aabbcc")
-                  .addField("Hello Faye! ", "Here's your floof for the day!")
-                  .setImage(jsonObject[0].url)
-                  .setTime();
-   
-  Hook.send(msg); 
-  console.log('>>>Message Sent');
+    console.log('>>>Body:', body);
+
+    var jsonObject = JSON.parse(body);
+
+    console.log('>>>URL:',jsonObject[0].url);
+    
+    //Webhook goes here
+    const Hook = new webhook.Webhook("https://discordapp.com/api/webhooks/550788590863515679/7Zdb2SAyW7p7hzmHdsLLHAXTZ8r2s5kFzuj1YezwCvYVPyJ98Eac0ULQcgi207ulCXuJ");
+    //Your Message goes here
+    const msg = new webhook.MessageBuilder()
+                    .setName("Pet of the Day")
+                    .setColor("#aabbcc")
+                    .addField("Hello Faye! ", "Here's your floof for the day!")
+                    .setImage(jsonObject[0].url)
+                    .setTime();
+    
+    Hook.send(msg); 
+    console.log('>>>Message Sent');
+
+  });
 
 });
-
-
-/*
-
-*/
